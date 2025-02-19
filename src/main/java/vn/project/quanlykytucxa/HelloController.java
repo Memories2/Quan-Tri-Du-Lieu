a672c93b11dcd3f8b6d766628b9011f82c962a55
Memories2 <dothanhtai555@gmail.com>
2025-02-19 21:01:57 +0700

    update tao base p2

--- src/main/java/vn/project/quanlykytucxa/HelloController.java
+++ src/main/java/vn/project/quanlykytucxa/HelloController.java
@@ -1,5 +1,6 @@
 package vn.project.quanlykytucxa;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
