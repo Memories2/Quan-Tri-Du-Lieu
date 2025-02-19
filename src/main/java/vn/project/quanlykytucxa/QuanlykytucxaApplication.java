f40bee81f75ac734334685aed33416b74a067ff6
Memories2 <dothanhtai555@gmail.com>
2025-02-19 20:45:10 +0700

    tao base

---
+++ src/main/java/vn/project/quanlykytucxa/QuanlykytucxaApplication.java
@@ -0,0 +1,13 @@
+package vn.project.quanlykytucxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlykytucxaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlykytucxaApplication.class, args);
+	}
+
+}
